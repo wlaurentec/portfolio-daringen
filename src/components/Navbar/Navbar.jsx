b892e886a4394b38c47fc4144bf81eda1ee54890
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-10-21 20:38:12 -0500

    add navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import "./Navbar.css"
+import logo from "../../assets/logo.svg"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src={logo} alt="" />
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Portolio</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">
+        Conncet With Me
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
