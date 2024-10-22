a9a66c928ace2c0a1d1342fac048ab6a6140295a
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-10-22 09:42:40 -0500

    add About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import Hero from "./components/Hero/Hero"
-import Navbar from "./components/Navbar/Navbar"
+import About from "./components/About/About";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
