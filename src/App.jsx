8487ab5ee120145371bee1f80bc5941bd57d4e30
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-10-22 11:30:32 -0500

    add my services

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About/About";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Services from "./components/Services/Services";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Services />
     </div>
   );
 };
